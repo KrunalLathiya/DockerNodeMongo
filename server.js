d7484fdacdfa47b0ddc4a4cfec3c8ad1cfc57efe
Krunal Lathiya <krunal@Krunals-MacBook-Air.local>
2018-04-15 00:26:34 +0530

    adddatabase

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
+const mongodb = require('mongodb');
 
 const config = require('./db');
 const PORT = 4000;
+const client = mongodb.MongoClient;
+
+client.connect(config.DB, function(err, db) {
+    if(err) {
+        console.log('database is not connected')
+    }
+    else {
+        console.log('connected!!')
+    }
+});
 
 app.get('/', function(req, res) {
     res.json({"hello": "world"});
